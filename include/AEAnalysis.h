a9e0f2158ebad5927d2dd1856e0c28017ebf459b
YLPeng <yunlou.peng@gmail.com>
2015-12-09 21:59:28 -0800

    udpate AE

--- include/AEAnalysis.h
+++ include/AEAnalysis.h
@@ -5,7 +5,7 @@
 #include "AEAnalysisLatticeNode.h"
 #include "llvm/IR/Constants.h"
 #include <string>
-
+#include <sstream>
 
 using namespace llvm;
 using namespace std;
@@ -13,8 +13,9 @@ using namespace std;
 class AEAnalysis: public BasicAnalysis{
 
 public:
+
     AEAnalysis(Function &F):BasicAnalysis(F){
-        errs()<<"============================>>AEAnalysis start.\n";
+        errs()<<"AEAnalysis start.\n";
         createCFG(F);
     };
 
@@ -27,13 +28,13 @@ public:
         Instruction *currentInst = currentNode->inst;
 
         string opName = currentInst->getOpcodeName();
-
+        errs()<< "string name: " << opName << '\n';
         if(opName == "add" || opName == "sub" || opName == "mul") {
             returnIn = analyzeAOpB(inputIn, currentInst);
         } else if (opName == "other") {
             //what should do here?
         } else { //there's no Operator? >>  F x:= y (in) ==
-            //do sth here.
+            return in;
         } 
 
      //left hand side of the statement
@@ -44,7 +45,9 @@ public:
 
 
     LatticeNode *latticeNodeInit(){
-        return new LatticeNode();
+        /* * ae = new AELatticeNode();
+        ae->val = new map<string, string>;*/
+        return new AEAnalysisLatticeNode(AEAnalysisLatticeNode::BOTTOM);
     }
 
     
@@ -53,8 +56,8 @@ public:
 
 private:
     AEAnalysisLatticeNode *analyzeAOpB(AEAnalysisLatticeNode *in, Instruction *currentInst){
-        errs() << "entering AEAnalysis::analyzeAOpB ==========================================<<" << '\n' ;
-
+        errs() << ">>  AEAnalysis::analyzeAOpB  <<" << '\n' ;
+        //map<string, string> value;
         AEAnalysisLatticeNode * newIn = new AEAnalysisLatticeNode(in);
 
         string opName = currentInst->getOpcodeName();
@@ -62,16 +65,67 @@ private:
         Value* leftOperandValue = currentInst->getOperand(0);
         string leftOperandStr = string(leftOperandValue->getName());
 
+        //errs() << "This left operand string is: " << leftOperandValue->getName() << '\n';
+
         Value* rightOperandValue = currentInst->getOperand(1);
         string rightOperandStr = string(rightOperandValue->getName());
-        
-        string currentInstLeftside = currentInst->getName();
+        string currentInstName = currentInst->getName(); //get inst name e.g. $add
+        currentInstName = "%" + currentInstName;
+        //errs() << "This instruction is: " << opName << " This node is: " << currentInstName << " = " << leftOperandStr << " " << opName << " " << rightOperandStr << "\n";
 
-        errs() << "This node is: " << currentInstLeftside << " = " << leftOperandStr << " " << opName << " " << rightOperandStr << "\n";
+        // Checking if left operand is a constant
+        if (ConstantInt *CILeft = dyn_cast<ConstantInt>(leftOperandValue)){
+            float leftVal = CILeft->getZExtValue();
+            stringstream ss (stringstream::in | stringstream::out);
+            ss << leftVal;
+            string leftValStr = ss.str();
+            errs() << "This leftVal is: " << leftVal << " The leftValStr is " << leftValStr <<'\n';
+
+            if (ConstantInt* CIRight = dyn_cast<ConstantInt>(rightOperandValue)) {
+                float rightVal = CIRight->getZExtValue();
+                stringstream ss (stringstream::in | stringstream::out);
+                ss << rightVal;
+                string rightValStr = ss.str();
+                //errs() << "This rightVal is: " << rightValStr << '\n';
+                errs() << "This instruction is: " << opName << " This node is: " << currentInstName << " = " << leftValStr << " " << opName << " " << rightValStr << "\n";
+                
+                if (newIn->val.find(opName)==newIn->val.end() && 
+                    newIn->val.find(leftValStr)==newIn->val.end() &&
+                    newIn->val.find(rightValStr)==newIn->val.end()) {
+                    errs() << " ADD op has not been found +=======+==+++++++++===++==+++=+++==++==++==+++===" << '\n';
+                    
+                    AEAnalysisLatticeNode* temp_1 = new AEAnalysisLatticeNode();
+                    newIn->val[currentInstName] = leftValStr + opName + rightValStr;
+                    temp_1->val = newIn->val;
+
+                    AEAnalysisLatticeNode* temp_2 = static_cast<AEAnalysisLatticeNode*>(temp_1->joinWith(newIn));
+                    delete temp_1;
+                    delete newIn;
+                    newIn = temp_2;
+                    errs() << " FINISH ADD op has not been found +=======+==+++++++++===++==+++=+++==++==++==+++===" << '\n';
+                }
+
+            } else { //left is constant but not right side one
+
+            }
+
+        } else { //both are not constant
+
+        }
+       
+        return newIn;
+    }
+
+};
+
+
+#endif
+
+
 
         //http://llvm.org/docs/doxygen/html/namespacellvm_1_1mdconst.html
-
-        if (currentInstLeftside != 0) {
+        /*
+        if (currentInstName != 0) {
         //if ((ConstantInt* CILeft = dyn_cast<ConstantInt>(leftOperandValue)) && (ConstantInt* CIRight = dyn_cast<ConstantInt>(rightOperandValue))) {
         //both left right sides of op are constants
 
@@ -83,26 +137,17 @@ private:
             //first  in - {x->*}
             for(map<string, string>::iterator it = newIn->val.begin(); it != newIn->val.end(); it++) {
                 //string inNodeRightOp = it->second;
-                if( currentInstLeftside == it->first ) { // x->* found in the latticeNode, remove it!
+                if( currentInstName == it->first ) { // x->* found in the latticeNode, remove it!
                     newIn->val.erase(it);
 
                 }else if( rightOperandStr == it->second || leftOperandStr == it->second ) { //same key different value found //delete everything x assign to.
                     newIn->val.erase(it);
 
                 }else { // ok to merge
-                    newIn->val[currentInstLeftside] = "";
+                    newIn->val[currentInstName] = "";
                 }
             } 
             //second in - {*->x}
 
         }
-
-        
-        return newIn;
-    }
-
-};
-
-
-
-#endif
+        */
--- include/AEAnalysisLatticeNode.h
+++ include/AEAnalysisLatticeNode.h
@@ -18,6 +18,7 @@
 #include <climits>
 #include <map>
 #include <algorithm>
+#include <string>
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -27,7 +28,6 @@ class AEAnalysisLatticeNode: public LatticeNode {
 public:
 
     map<string, string> val;
-
 
     bool equalsTo(LatticeNode *other) {
 
@@ -60,18 +60,30 @@ public:
 
     LatticeNode * joinWith(LatticeNode *other) {
         AEAnalysisLatticeNode * otherNode = static_cast<AEAnalysisLatticeNode*>(other);
-
-        if (this->basic == BOTTOM && otherNode->basic == BOTTOM) { return new AEAnalysisLatticeNode(BOTTOM); }
+        errs()<< "I just entered into the sublcassed join... \n";
         
-        if (this->basic == TOP || otherNode->basic == TOP) { return new AEAnalysisLatticeNode(TOP); }
+        if (this->basic == BOTTOM && otherNode->basic == BOTTOM) { 
+            errs()<< "I just entered 1... \n"; 
+            return new AEAnalysisLatticeNode(BOTTOM); 
+        }
         
-        if (this->basic == BOTTOM) { return new AEAnalysisLatticeNode(otherNode); }
-        else if (otherNode->basic == BOTTOM) { return new AEAnalysisLatticeNode(this); }
+        if (this->basic == BOTTOM) { 
+            "I just entered 2... \n"; 
+            return new AEAnalysisLatticeNode(otherNode); 
+        }else if (otherNode->basic == BOTTOM) { 
+            "I just entered 3... \n";
+            return new AEAnalysisLatticeNode(this);
+        }
+        
+        if (this->basic == TOP || otherNode->basic == TOP) { 
+            "I just entered 4... \n";
+            return new AEAnalysisLatticeNode(TOP); 
+        }
         
         //common situation
         AEAnalysisLatticeNode *newNode = new AEAnalysisLatticeNode();
         newNode->val = this->val;
-        
+        errs()<< "size of newNode-val :>>" << newNode->val.size() << " \n";
         for(map<string, string>::iterator it = newNode->val.begin(); it != newNode->val.end(); it++) {
             if(otherNode->val.find(it->first) == otherNode->val.end()) { 
                 //no same key found, merge(intersect) both nodes to empty!
@@ -94,16 +106,17 @@ public:
         return newNode;
     }
 
-    AEAnalysisLatticeNode(){} // init
+    AEAnalysisLatticeNode():LatticeNode(){} // init
     AEAnalysisLatticeNode(string s):LatticeNode(s){}// initialize with TOP or BOTTOM
-    AEAnalysisLatticeNode(AEAnalysisLatticeNode *node){
-        errs()<<"AEAnalysisLatticeNode::AEAnalysisLatticeNode good!\n";
+    AEAnalysisLatticeNode(AEAnalysisLatticeNode *node):LatticeNode(AEAnalysisLatticeNode(node->basic)){
+        errs()<<"00AEAnalysisLatticeNode::AEAnalysisLatticeNode good!!\n";
         this->basic = node->basic;
-        errs()<<node->val.size();
-        errs()<<"AEAnalysisLatticeNode::AEAnalysisLatticeNode good!!\n";
+        errs()<<"11AEAnalysisLatticeNode::AEAnalysisLatticeNode good!!\n";
+        errs()<< "this->val size: " << this->val.size() << '\n';
         this->val = node->val;
         errs()<<"AEAnalysisLatticeNode::AEAnalysisLatticeNode good!!!\n";
     }
+    
     ~AEAnalysisLatticeNode(){}
 
 
